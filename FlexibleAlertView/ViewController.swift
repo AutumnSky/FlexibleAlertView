5ec2730b6c74ef83de2d9452f0e1ee1bfc0a580c
MinJeong Kim <capu1004@gmail.com>
2018-08-18 12:57:18 +0900

    Initial Commit

---
+++ FlexibleAlertView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlexibleAlertView
+//
+//  Created by MinJeong Kim on 2018. 8. 18..
+//  Copyright © 2018년 asfy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
